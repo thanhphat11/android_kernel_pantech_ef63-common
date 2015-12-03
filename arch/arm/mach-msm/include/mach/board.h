b09cdf2891df871602e4200d9f303c9acca6647a
mifl <mifl.pc@gmail.com>
2015-12-03 17:08:36 +0100

    msm: camera: Add Pantech modifications
    Change-Id: Iab9d8b10de58bc86dca8d5469564a427caea9067

--- arch/arm/mach-msm/include/mach/board.h
+++ arch/arm/mach-msm/include/mach/board.h
@@ -190,7 +190,11 @@ struct msm_gpio_set_tbl {
 
 struct msm_camera_gpio_num_info {
 #ifdef CONFIG_MSMB_CAMERA_LEGACY
+#ifdef CONFIG_PANTECH_CAMERA
+	uint16_t gpio_num[11];
+#else
 	uint16_t gpio_num[7];
+#endif
 #else
 	uint16_t gpio_num[10];
 	uint8_t valid[10];
