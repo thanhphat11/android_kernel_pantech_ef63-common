4b66501ce34fb3f3b70f645a14731ca6f47ddcbd
mifl <mifl.pc@gmail.com>
2015-12-03 18:57:59 +0100

    pantech: ef63: Initial board files
    Change-Id: I1882a790fd7a0fa8d76140ee98665bc9e1ce4e20

---
+++ include/pantech/BOARD_VER.h
@@ -0,0 +1,12 @@
+#ifndef BOARD_VER_H
+#define BOARD_VER_H
+
+#if defined(CONFIG_MACH_MSM8974_EF63K)
+#include "BOARD_VER_EF63K.h"
+#elif defined(CONFIG_MACH_MSM8974_EF63L)
+#include "BOARD_VER_EF63L.h"
+#elif defined(CONFIG_MACH_MSM8974_EF63S)
+#include "BOARD_VER_EF63S.h"
+#endif
+
+#endif /*BOARD_VER_H*/
